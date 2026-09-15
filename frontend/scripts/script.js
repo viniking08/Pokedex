ba3efd1aed53b2340867364359efd442e784ed91
viniking08 <vinitransformers@hotmail.com>
2026-09-14 21:41:55 -0300

    base do modal
    base de modal e algumas pequenas adições

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,5 +1,6 @@
 const pokemonGrid = document.getElementById("pokemonGrid");
 let allPokemon = [];
+const pokemonClicado = allPokemon.find(i => i.name === event.target.dataset.pokemon);
 
 async function buscarPokemons() {
     try {
@@ -22,19 +23,45 @@ async function buscarPokemons() {
 
 function renderizarPokemons() {
     allPokemon.forEach(i => {
-        console.log(i)
         pokemonGrid.innerHTML += `
         <div class="card" style="width: 18rem;">
         <img class="img-card" src="${i.sprites.other["official-artwork"].front_default}" class="card-img-top" alt="${i.name}">
         <div class="card-body">
             <h5 class="card-title">${i.name}</h5>
             <p class="card-text">No. ${i.id}</p>
-            <a href="#" class="btn btn-primary">Ver Detalhes</a>
+            <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalPokemon" data-pokemon="${i.name}">
+    Ver Detalhes
+</a>
         </div>
     </div>
         `
     })
 }
 
-buscarPokemons();
+pokemonGrid.addEventListener("click", (event) => { // ativa clicando em qualquer lugar mas só funciona no botao
+    if (event.target.dataset.pokemon) {
 
+        const pokemonClicado = allPokemon.find(i => i.name === event.target.dataset.pokemon);
+
+        document.querySelector("#nomePokemon").textContent = pokemonClicado.name;
+
+        document.querySelector("#imagemPokemon").src = pokemonClicado.sprites.other["official-artwork"].front_default;
+
+        document.querySelector("#tipoPokemon").textContent = pokemonClicado.types.map(tipo => tipo.type.name).join(" / ");
+
+        document.querySelector("#hpPokemon").textContent = pokemonClicado.stats[0].base_stat;
+
+        document.querySelector("#ataquePokemon").textContent = pokemonClicado.stats[1].base_stat;
+
+        document.querySelector("#defesaPokemon").textContent = pokemonClicado.stats[2].base_stat;
+
+        document.querySelector("#velocidadePokemon").textContent = pokemonClicado.stats[5].base_stat;
+
+        document.querySelector("#alturaPokemon").textContent = pokemonClicado.height / 10 + " m";
+
+        document.querySelector("#pesoPokemon").textContent = pokemonClicado.weight / 10 + " kg";
+
+    }
+});
+
+buscarPokemons();
